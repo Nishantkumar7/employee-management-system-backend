392d52a132457a60729027e11d894b00c933a34d
Nishantkumar7 <niskum2002@gmail.com>
2025-06-16 09:01:42 +0530

    fix: allow deployed Angular frontend in CORS config

--- src/main/java/com/example/employeemanager/config/WebConfig.java
+++ src/main/java/com/example/employeemanager/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://employee-management-system-u5f5.onrender.com","http://localhost:4200")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);;
